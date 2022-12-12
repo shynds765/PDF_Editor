f7fa79e0374b17cf722dac73c02449f2fb758f26
shynds765 <shynds765@gmail.com>
2022-12-12 16:29:14 -0500

    Worked on Uploading PDF

--- Add_PDF.js
+++ Add_PDF.js
@@ -1,3 +1,12 @@
-function Add_PDF() {
+function Upload_PDF() {
     document.getElementById('file').click()
+    document.getElementById('file').addEventListener('change', Add_PDF);
+}
+
+function Add_PDF() {
+    let reader = new FileReader()
+    const file = this.files[0];
+
+    reader.readAsArrayBuffer(file);
+    const pdf = PDFDocument.load(reader.result);
 }
